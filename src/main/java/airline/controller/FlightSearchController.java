d4f0889a2cf60083bb1926d8e8b74619941eb6f5
Riddhi Agrawal <riddhi.agrawal91@gmail.com>
2017-09-01 11:41:17 +0530

    Story 0

---
+++ src/main/java/airline/controller/FlightSearchController.java
@@ -0,0 +1,39 @@
+package airline.controller;
+
+import airline.model.City;
+import airline.model.Flight;
+import airline.repositories.CityRepository;
+import airline.services.FlightSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+
+@Controller
+public class FlightSearchController {
+
+
+    CityRepository cityRepository;
+    @Autowired
+    FlightSearchService flightSearchService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getCities(Model model) {
+        cityRepository = new CityRepository();
+        List<City> cities = cityRepository.getCities();
+        model.addAttribute("cities", cities);
+        model.addAttribute("flight", new Flight("F1","HYD","BLR"));
+        return "FlightSearch";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public String getFlights(@ModelAttribute(value = "flight") Flight flight, Model model) {
+        flightSearchService = new FlightSearchService();
+        List<Flight> availableFlights = flightSearchService.search(flight.getSource(), flight.getDestination());
+        model.addAttribute("searchResults",availableFlights);
+        return "FlightsList";
+    }
+}
